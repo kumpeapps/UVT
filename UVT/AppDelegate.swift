b94ad244757f0cd1c3287f9237c5f23caf29c376
Justin Kumpe <jakumpe@kumpes.com>
2022-07-24 18:57:31 -0500

    init

--- UVT/AppDelegate.swift
+++ UVT/AppDelegate.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import Keys
+import NewRelic
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        NewRelic.start(withApplicationToken:"\(UVTKeys().newrelic_token)")
         return true
     }
 
@@ -31,6 +32,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
