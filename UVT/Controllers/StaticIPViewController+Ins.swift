cd01cc594fcfce3960cdb7cd1f65d52f1fb198b9
Justin Kumpe <jakumpe@kumpes.com>
2022-07-29 21:19:20 -0500

    Update StaticIPViewController+Ins.swift
    #TestFlight #beta

--- UVT/Controllers/StaticIPViewController+Ins.swift
+++ UVT/Controllers/StaticIPViewController+Ins.swift
@@ -11,7 +11,7 @@ import KumpeHelpers
 extension StaticIPViewController {
     // MARK: buildAARIS
     @objc func buildAARIS(sender: UIButton) {
-        instructions = "AARIS Gateway Instructions\n\n1. Goto the RG configuration Page (192.168.1.254)\n\n2. Click Home Network\n\n3. Click Subnets & DHCP\n\n4. Set Public Subnet Enable to On\n\n5. In public IPv4 Address Field Enter \(ipInfo!.defaultGateway)\n\n6. In the Public Subnet mask Enter \(ipInfo!.subnetMask)\n\n7. In the Public DHCPv4 Start Address Field Enter \(ipInfo!.firstUsable.string)\n\n8. In the Public DHCPv4 End Address Field Enter \(ipInfo!.lastUsable.string)\n\n9. Set Allow Inbound Traffic to On\n\n10. Set Primary DHCP Pool to Private\n\n11. Click Save"
+        instructions = "AARIS Gateway Instructions\n\n1. Goto the RG configuration Page (192.168.1.254)\n\n2. Click Home Network\n\n3. Click Subnets & DHCP\n\n4. Set Public Subnet Enable to On\n\n5. In public IPv4 Address Field Enter \(ipInfo!.defaultGateway.string)\n\n6. In the Public Subnet mask Enter \(ipInfo!.subnetMask)\n\n7. In the Public DHCPv4 Start Address Field Enter \(ipInfo!.firstUsable.string)\n\n8. In the Public DHCPv4 End Address Field Enter \(ipInfo!.lastUsable.string)\n\n9. Set Allow Inbound Traffic to On\n\n10. Set Primary DHCP Pool to Private\n\n11. Click Save"
         labelText.text = instructions
     }
 
